a38cde79f52581f0d3de1a2d788cb01331a85913
Kamal Krishna Ghosh <kamal krishna ghosh>
2025-06-08 19:03:39 +0530

    Initial commit

---
+++ kkg/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./components/Card.jsx"; // Add './' to match relative path
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-amber-300 text-black rounded-xl text-center p-2 mb-3">kamal</h1>
+      <Card username = "kamal"/>
+    </>
+  );
+}
+
+export default App;
